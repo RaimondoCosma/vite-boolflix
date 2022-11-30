cd6ee2a1b744e71a2141b853a93a01042ea15f83
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-30 12:09:08 +0100

    Crea cartella main con componenti card

---
+++ src/components/main/AppCardMovies.vue
@@ -0,0 +1,18 @@
+<script>
+import { store } from "../../store";
+
+export default {
+  name: "AppCardMovies",
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/main/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from "../../store";
+import AppCardMovies from "./AppCardMovies.vue";
+import AppCardSeries from "./AppCardSeries.vue";
+
+export default {
+  name: "AppMain",
+  components: {
+    AppCardMovies,
+    AppCardSeries,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
